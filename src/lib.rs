bcdf5b0f33ff9184ab114211882711c60e2c0379
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-08-27 06:28:04 +0900

    Print to VGA test buffer.

--- src/lib.rs
+++ src/lib.rs
@@ -2,24 +2,20 @@
 #![feature(asm)]
 #![no_std]
 
-extern crate volatile;
-use volatile::Volatile;
-
-static HELLO: &[u8] = b"Hello from Rust!\n";
+static HELLO: &[u8] = b"Hello from Rust!";
 
 #[no_mangle]
 pub extern fn rust_main() {
-  let uart16550 = 0x10000000 as *mut Volatile<u8>;
+  let vga_buffer = 0xb8000 as *mut u16;
 
-  for &byte in HELLO.iter() {
+  for (i, &byte) in HELLO.iter().enumerate() {
     unsafe {
-      (*uart16550).write(byte);
+      *vga_buffer.offset(i as isize) = 0x3u16 << 8 | byte as u16;
     }
   }
 }
 
 use core::panic::PanicInfo;
-
 #[panic_implementation]
 #[no_mangle]
 pub fn panic(_info: &PanicInfo) -> ! {
